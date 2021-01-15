d35ddfd289d5084cd5185e94eb38aed9047280dc
Natee <gg977120@gmail.com>
2021-01-15 15:18:26 +0700

    write functoin fibonacci

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x)
+{
+    switch (x)
+    {
+    case 0: return 0;
+    case 1: return 1;
+    default: return fibonacci(x-1) + fibonacci(x-2);
+    }
 }
