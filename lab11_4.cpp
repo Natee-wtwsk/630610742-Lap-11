4bfcc30cdf281a734fdd4471a313006c3c96d0a3
Natee <gg977120@gmail.com>
2021-01-15 17:35:49 +0700

    modified mySwap for function overload

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
+
+template <typename T>
+void mySwap(T &x,T &y);
 
 int main(){
 	int x, y;
@@ -30,3 +33,11 @@ int main(){
 	return 0;
 }
 
+template <typename T>
+void mySwap(T &x,T &y)
+{
+	T n;
+	n = y;
+	y = x;
+	x = n;
+}
